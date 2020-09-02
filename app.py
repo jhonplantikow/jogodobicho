f28cd329912306419a1ee6713ce6ed8f19d2c222
root <root@DESKTOP-G99TUN4.localdomain>
2020-09-02 01:00:05 -0300

    Criado lista de bicho, atualizado metodo de coleta dos resultados

--- app.py
+++ app.py
@@ -16,6 +16,35 @@ td = table.findAll("td")
 
 os.system('clear') or None
 
+lstNumeroBicho = [["1","Avestruz"],
+["2","Aguia"],
+["3","Burro"],
+["4","Borboleta"],
+["5","Cachorro"],
+["6","Cabra"],
+["7","Carneiro"],
+["8","Camelo"],
+["9","Cobra"],
+["10","Coelho"],
+["11","Cavalo"],
+["12","Elefante"],
+["13","Galo"],
+["14","Gato"],
+["15","Jacaré"],
+["16","Leão"],
+["17","Macaco"],
+["18","Porco"],
+["19","Pavão"],
+["20","Peru"],
+["21","Touro"],
+["22","Tigre"],
+["23","Urso"],
+["24","Veado"],
+["25","Vaca"]]
+
+for f in lstNumeroBicho:
+    print(f[0], " - ", f[1])
+
 lstResultado = ["PTM","PT","PTV","PTN","COR"]
 
 for r in range(0, len(lstResultado)):
